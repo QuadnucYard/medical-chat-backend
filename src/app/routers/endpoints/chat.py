c785a0b0eb4a842e435b4aaeb7630ef34da9a948
QuadnucYard <2380433991@qq.com>
2023-09-11 21:16:27 +0800

    Add superuser guard back to stats

--- src/app/routers/endpoints/chat.py
+++ src/app/routers/endpoints/chat.py
@@ -14,7 +14,7 @@ router = APIRouter()
 async def get_chat_stats(
     *,
     db: AsyncSession = Depends(deps.get_db),
-    # user: models.User = Depends(deps.get_current_active_superuser),
+    user: models.User = Depends(deps.get_current_active_superuser),
 ):
     return await chat_service.get_stats(db)
 
